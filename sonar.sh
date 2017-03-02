a56234b9ec9c0775b1597f821bc746ee263db001
ka-hirose <ka-hirose@kddi-tech.com>
2017-03-02 14:15:18 +0900

    123

--- sonar.sh
+++ sonar.sh
@@ -8,6 +8,6 @@ GITHUB_OAUTH=$5
 
 if [ $PULLS_NUMBER = 'false' ]; then
   exit
-fi
+fi  
 
 mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.0.1:sonar -Dmaven.test.skip=true -Dclirr=true -Dsonar.host.url=${SONAR_HOST} -Dsonar.login=${SONAR_USERNAME} -Dsonar.password=${SONAR_PASSWORD} -Dsonar.sourceEncoding=UTF-8 -Dsonar.analysis.mode=issues -Dsonar.github.pullRequest=${PULLS_NUMBER} -Dsonar.github.oauth=${GITHUB_OAUTH} -Dsonar.github.repository=ka-hirose/SonarQubeTest -B -e -V
